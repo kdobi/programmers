6549d6ae7806f8dffc4b31fa332491df3bfe9edb
김동빈 <kdobi87@naver.com>
2026-06-24 22:24:55 +0900

    지폐 접기

---
+++ src/지폐접기/Solution.java
@@ -0,0 +1,24 @@
+package 지폐접기;
+
+class Solution {
+    public int solution(int[] wallet, int[] bill) {
+        int answer = 0;
+
+        while(wallet[0] < bill[0] || wallet[1] < bill[1]) {
+
+            if(bill[0] > bill[1]) {
+                bill[0] /= 2;
+            }
+
+            else {
+                bill[1] /= 2;
+            }
+
+            answer++;
+
+            if(bill[0] <= wallet[1] && bill[1] < wallet[0]) break;
+        }
+
+        return answer;
+    }
+}
